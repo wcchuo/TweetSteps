283c971d98ca8037a1511d8cf7b43d8edff762d7
wcchuo <wcchuo@yahoo.com>
2015-08-13 16:35:40 -0700

    Initial Commit

---
+++ Twipper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twipper
+//
+//  Created by Wei Chung Chuo on 8/13/15.
+//  Copyright © 2015 Wei Chung Chuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TwipperTests/TwipperTests.swift
@@ -0,0 +1,36 @@
+//
+//  TwipperTests.swift
+//  TwipperTests
+//
+//  Created by Wei Chung Chuo on 8/13/15.
+//  Copyright © 2015 Wei Chung Chuo. All rights reserved.
+//
+
+import XCTest
+@testable import Twipper
+
+class TwipperTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
